0edee66e6490914f7f71944840b23189c134b437
Sarath S Nair <SNair0@walmart.com>
2019-01-03 23:42:26 +0530

    Solved maxchar problem by creating a character-count object

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let maxChar,
+    maxCount = 0;
+  let obj = str.split("").reduce((obj, char) => {
+    obj[char] = obj[char] + 1 || 1;
+    if (obj[char] && obj[char] > maxCount) {
+      maxCount = obj[char];
+      maxChar = char;
+    }
+    return obj;
+  }, {});
+
+  return maxChar;
+}
 
 module.exports = maxChar;
